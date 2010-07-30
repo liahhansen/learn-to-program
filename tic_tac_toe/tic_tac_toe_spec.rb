84b24a7c4e5371137a0fdecf70599a147dcb3395
Liah Hansen <liahhansen@gmail.com>
2010-07-29 19:53:48 -0700

    updated for classes

--- tic_tac_toe/tic_tac_toe_spec.rb
+++ tic_tac_toe/tic_tac_toe_spec.rb
@@ -1,20 +1,20 @@
 require 'tic_tac_toe'
-describe "TicTacToe" do
+describe TicTacToe do
   before do
-    start_game
+    @tic_tac_toe = TicTacToe.new
   end
   
   it "has a name" do
-    name.should == "Tic Tac Toe"
+    @tic_tac_toe.name.should == "Tic Tac Toe"
   end
   
   it "should initially have 9 empty 'squares'" do
-    game_status.should == [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+    @tic_tac_toe.game_status.should == [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
   end
   
   it "#play should allow player to make a mark on the board" do
-    play(1,2,'x').should == [["-", "-", "-"], ["-", "-", "x"], ["-", "-", "-"]]
-    play(1,0,'o').should == [["-", "-", "-"], ["o", "-", "x"], ["-", "-", "-"]]
+    @tic_tac_toe.play(1,2,'x').should == [["-", "-", "-"], ["-", "-", "x"], ["-", "-", "-"]]
+    @tic_tac_toe.play(1,0,'o').should == [["-", "-", "-"], ["o", "-", "x"], ["-", "-", "-"]]
   end
   
 end
