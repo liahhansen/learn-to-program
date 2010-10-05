d1f69356bf5a1e98d64e3f55ce5ed286db00da85
Liah Hansen <liahhansen@gmail.com>
2010-10-05 14:59:59 -0700

    made new branch for solutions

--- translator/translator.rb
+++
@@ -1,9 +0,0 @@
-class Translator
-  def to_english
-    {}
-  end
-  
-  def to_foreign
-    {:spanish => {"hello" => "hola", "goodbye" => "adios"}, :french => {"hello" => "bonjour", "goodbye" => "au revior"}, :japanese => {"hello" => "konnichiha", "goodbye" => "sayonara"}, :swahili => {"hello" => "jambo", "goodbye" => "kwaheri"}}
-  end
-end
